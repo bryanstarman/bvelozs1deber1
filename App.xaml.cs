9c6a76e25f35e1902e6d430389271720e14e855f
bryanstarman <67846938+bryanstarman@users.noreply.github.com>
2024-04-08 08:37:48 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace bvelozt1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace bvelozt1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void calcular(object sender, EventArgs e)
+        {
+           
+            String nombre=txtNombre.Text;
+            String apellido=txtApellido.Text;
+            String edad=txtEdad.Text;
+            double sueldo=Convert.ToDouble(txtSalario.Text);
+            double iess = (sueldo * 9.45) / 100;
+            DisplayAlert("Alerta", "Bienvenido \n" + nombre + " " + apellido + "\n" + "Edad: " + edad+"\nAporte IESS: "+iess,"Cerrar");
+        }
+    }
+    }
